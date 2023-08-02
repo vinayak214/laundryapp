6dbf45b1338db684f26ab9fd29adade31bd70b6d
vinayak <v4vinupa@email.com>
2023-08-02 15:12:42 -0500

    updating the location permission

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,27 +9,49 @@ const HomeScreen = () => {
     const [longitude, setLongitude] = useState(null);
     const [address, setAddress] = useState(null);
 
+
     useEffect(() => {
-        // Get the current location (latitude and longitude)
-        Geolocation.getCurrentPosition(
-            (position) => {
+        // Request location permission
+        requestLocationPermission();
+      }, []);
+
+      const requestLocationPermission = async () => {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            {
+              title: 'Location Permission',
+              message: 'This app needs access to your location to fetch data.',
+            }
+          );
+    
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            // Get the current location (latitude and longitude)
+            Geolocation.getCurrentPosition(
+              (position) => {
                 const { latitude, longitude } = position.coords;
                 setLatitude(latitude);
                 setLongitude(longitude);
-
+    
                 // Perform reverse geocoding to get the address
                 Geocoder.geocodePosition({ lat: latitude, lng: longitude })
-                    .then((results) => {
-                        if (results.length > 0) {
-                            setAddress(results[0].formattedAddress);
-                        }
-                    })
-                    .catch((error) => console.warn(error));
-            },
-            (error) => console.warn(error),
-            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-        );
-    }, []);
+                  .then((results) => {
+                    if (results.length > 0) {
+                      setAddress(results[0].formattedAddress);
+                    }
+                  })
+                  .catch((error) => console.warn(error));
+              },
+              (error) => console.warn(error),
+              { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+            );
+          } else {
+            console.log('Location permission denied');
+          }
+        } catch (error) {
+          console.warn('Error requesting location permission:', error);
+        }
+      };   
 
     return (
         <View style={styles.container}>
